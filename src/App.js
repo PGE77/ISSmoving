21e35417c6be10f2be71e0795a5f5311d3ae1bb7
Petr Gargulák <gargulakpetr@seznam.cz>
2023-01-25 16:19:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [urlMap, setUrlMap] = useState("");
+  const url = "http://api.open-notify.org/iss-now.json";
+
+  const getCoordinates = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const iss_long = data.iss_position.longitude;
+    const iss_lati = data.iss_position.latitude;
+    setTimeout(() => {
+      setLatitude(data.iss_position.latitude);
+      setLongitude(data.iss_position.longitude);
+
+      setUrlMap(
+        `https://frame.mapy.cz/zakladni?x=${iss_long}&y=${iss_lati}&z=1&source=coor&&id=${iss_long}%2C${iss_lati}`
+      );
+    }, 5000);
+  };
+  useEffect(() => {
+    getCoordinates();
+  });
+
+  return (
+    <div>
+      <h1>Zeměpisná šířka: {latitude}</h1>
+      <h1>Zeměpisná délka: {longitude}</h1>
+
+      <iframe
+        title="ttest"
+        src={urlMap}
+        width="800"
+        height="600"
+      
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
