eff7cf38c20dfd164177c52c9ee8a234e165266c
Petr Gargulák <gargulakpetr@seznam.cz>
2023-01-25 17:24:14 +0100

    popis commitu

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ const App = () => {
     const requestOptions = {
       method: "POST",
       mode: "no-cors",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(this.tutorial)
+      headers: { "Content-Type": "application/json" }
   } 
     const response = await fetch(url , requestOptions);
     const data = await response.json();
