9ca83c828d57d4e0f58b9cbb080807d37b2f80ff
Petr Gargulák <gargulakpetr@seznam.cz>
2023-01-25 17:37:19 +0100

    popis commitu

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ const App = () => {
 
   const getCoordinates = async () => {
     
-    const requestOptions = {
-      method: "POST",
-      mode: "no-cors",
-      headers: { "Content-Type": "application/json" }
+  //   const requestOptions = {
+  //     method: "POST",
+  //     mode: "no-cors",
+  //     headers: { "Content-Type": "application/json" }
 
-  } 
-    const response = await fetch(url , requestOptions);
+  // } 
+    const response = await fetch(url );
     const data = await response.json();
     const iss_long = data.iss_position.longitude;
     const iss_lati = data.iss_position.latitude;
-    setTimeout(() => {
+   
       setLatitude(data.iss_position.latitude);
       setLongitude(data.iss_position.longitude);
 
       setUrlMap(
         `https://frame.mapy.cz/zakladni?x=${iss_long}&y=${iss_lati}&z=1&source=coor&&id=${iss_long}%2C${iss_lati}`
       );
-    }, 5000);
+ 
   };
   useEffect(() => {
     getCoordinates();
-  });
+  }, []);
 
   return (
     <div>
