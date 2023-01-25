0c5499d13f05fb137356df2eb2656788efc56f38
Petr Gargulák <gargulakpetr@seznam.cz>
2023-01-25 18:46:46 +0100

    popis commitu

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ const App = () => {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [urlMap, setUrlMap] = useState("");
-  const url = "http://api.open-notify.org/iss-now.json";
+  const url = "https://api.wheretheiss.at/v1/satellites/25544";
 
   const getCoordinates = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    const iss_long = data.iss_position.longitude;
-    const iss_lati = data.iss_position.latitude;
+    const iss_long = data.longitude;
+    const iss_lati = data.latitude;
 
-    setLatitude(data.iss_position.latitude);
-    setLongitude(data.iss_position.longitude);
+    setLatitude(data.latitude);
+    setLongitude(data.longitude);
 
     setUrlMap(`https://frame.mapy.cz/zakladni?x=${iss_long}&y=${iss_lati}&z=1&source=coor&&id=${iss_long}%2C${iss_lati}`);
   };
